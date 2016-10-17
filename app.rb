5e266050423cd956bdcc169e1edfbf599888d50a
kuznetz <s.kuznetzov@mail.ru>
2016-10-17 19:09:00 +0300

    восстановление

--- app.rb
+++ app.rb
@@ -1,38 +1,45 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database,"sqlite3:barbershop.db"
+set :database, "sqlite3:barbershop.db"
 
-class Client < ActiveRecord::Base 
-	
+class Client < ActiveRecord::Base
 end
-class Barber < ActiveRecord::Base 
-	end
-get before  do
+
+class Barber < ActiveRecord::Base
+end
+
+before do
 	@barbers = Barber.all
-	end
+end
 
 get '/' do
 	erb :index			
 end
 
 get '/visit' do
-   erb :visit
+	erb :visit
 end
 
 post '/visit' do
 
-   @username = params[:username]
-    @phone = params[:phone]
-    @datetime = params[:datetime]
-   @barber = params[:barber]
-   @color = params[:color]
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+c = Client.new
+c.name =@username
+c.phone = @phone
+c.datestamp = @datetime
+c.barber = @barber
+c.color = @color
+c.save
 
 
-
-   erb "<h2>Спасибо, вы записались!</h2>"
+	erb "<h2>Спасибо, вы записались!</h2>"
 
 end
